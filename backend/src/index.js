852aef6553471981695a50d41fb581da13e7ade4
Joy <satyanandshreyash@gmail.com>
2025-01-07 20:05:01 +0530

    modify url

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,10 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: `${process.env.FRONTEND_URL}`,
+    credentials: true
+}));
 
 app.use('/api/user', authRoutes);
 
